3083ef243dedefad324425e8791daa4daf1ca84e
Tony <286348414@qq.com>
2017-03-02 22:58:39 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/dell/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.example.dell.coolweather.db;
+
+/**
+ * Created by dell on 2017/3/2.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
